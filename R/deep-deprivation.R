99679b3a4e18ddbe0406edb9c8bc7129406a935c
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2022-12-22 09:44:02 +0000

    Persistent deprivation in LBAs

--- R/deep-deprivation.R
+++ R/deep-deprivation.R
@@ -3,6 +3,7 @@ library(geographr)
 library(IMD)
 library(sf)
 
+# LSOA 2001 to LSOA 2011 lookup
 # Source: https://geoportal.statistics.gov.uk/datasets/ons::lower-layer-super-output-area-2001-to-lower-layer-super-output-area-2011-to-local-authority-district-2011-lookup-in-england-and-wales-1/about
 lookup_lsoa01_lsoa11 <- read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA01_LSOA11_LAD11_EW_LU_ddfe1cd1c2784c9b991cded95bc915a9/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson") |>
   st_drop_geometry()
@@ -78,3 +79,65 @@ imd_unchanged |>
 #--> North West, Yorkshire, West Mids
 # More than half of the most deprived areas of the North West, North East, Yorkshire, and West Mids in 2019 have been among the country's most deprived areas since 2004
 # Nearly one in five neighbourhoods in the North West have been among the nation's most deprived for at least 15 years
+
+# ---- Is persistent deprivation more likely in what are now known as left-behind areas? ----
+# LSOA to ward lookup
+# Source: https://geoportal.statistics.gov.uk/datasets/ons::lower-layer-super-output-area-2011-to-ward-2017-lookup-in-england-and-wales-1/about
+lookup_lsoa11_ward17 <- read_sf("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA11_WD17_EW_LU_c65dfe9ca2fa4b0fa578b65a4d5c9856/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
+
+lookup_lsoa11_ward17 <-
+  lookup_lsoa11_ward17 |>
+  st_drop_geometry() |>
+  select(lsoa11_code = LSOA11CD, ward17_code = WD17CD)
+
+#
+lba_trends <-
+  imd_trends |>
+  left_join(lookup_lsoa11_ward17) |>
+  left_join(IMD::cni_england_ward17) |>
+
+  mutate(
+    core20 = if_else(`2004` <= 2, "20% most deprived", "Less deprived areas"),
+    deprivation_unchanged = if_else(`2004` == `2007` & `2007` == `2010` & `2010` == `2015` & `2015` == `2019`, "Persistent", "Changing"),
+    lba = if_else(`Left Behind Area?`, "Left-behind area", "Other")
+  )
+
+lba_trends |>
+  janitor::tabyl(core20, deprivation_unchanged, lba)
+
+lba_trends |>
+  group_by(lba, core20, deprivation_unchanged) |>
+  summarise(n = n()) |>
+  mutate(prop = n / sum(n)) |>
+  ungroup() |>
+
+  drop_na() |>
+
+  mutate(core20 = fct_rev(core20)) |>
+
+  ggplot(aes(x = core20, y = prop)) +
+  geom_col(aes(fill = deprivation_unchanged)) +
+  facet_wrap(~lba) +
+  coord_flip() +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = c("#d7d8d7", "#ee2a24")) +
+  theme_minimal() +
+  theme(
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    legend.position = "top",
+    plot.background = element_rect(fill = "white"),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    plot.subtitle = element_text(colour = "#5c747a")
+  ) +
+  labs(
+    title = "Deprivation is most persistent in England's left behind' neghbourhoods",
+    subtitle = "'Deprivation status' is based on whether a small area's deprivation decile has \nchanged since 2004 ('Changing') or not ('Persistent').",
+    fill = "Deprivation status",
+    x = NULL,
+    y = NULL,
+    caption = "@matthewgthomas analysis of DLUHC and OCSI data"
+  )
+
+ggsave("output/persistent deprivation in left behind areas.png", width = 150, height = 100, units = "mm")
